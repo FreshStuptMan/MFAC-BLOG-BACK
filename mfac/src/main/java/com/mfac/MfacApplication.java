dfa6b4bbfdff4b609f102c4e4cc39ce549103062
FreshStuptMan <1367213168@qq.com>
2024-07-06 14:11:33 +0800

    项目搭建完成

---
+++ mfac/src/main/java/com/mfac/MfacApplication.java
@@ -0,0 +1,13 @@
+package com.mfac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MfacApplication.class, args);
+    }
+
+}
---
+++ mfac/src/main/java/com/mfac/config/WebMvcConfiguration.java
@@ -0,0 +1,75 @@
+package com.mfac.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Component
+@Slf4j
+public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+
+
+
+
+    /**
+     * 通过knife4j生成接口文档
+     * @return
+     */
+    @Bean
+    public Docket docket() {
+        Contact contact = new Contact(
+                "FreshStuptMan", "", ""     // 作者姓名
+        );
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("MFAC接口文档")
+                .version("1.0")
+                .contact(contact)
+                .description("MFAC接口文档")
+                .build();
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.mfac.controller"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
+
+    /**
+     * 设置静态资源映射
+     * @param registry
+     */
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    /**
+     * 扩展 Spring MVC 消息转换器 统一消息格式
+     * @param converters
+     */
+//    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        log.info("扩展消息转换器...");
+//        // 创建一个消息转换器对象
+//        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+//        // 为该消息转换器设置一个对象转换器(将java对象按指定格式序列化为json数据)
+//        converter.setObjectMapper(new JacksonObjectMapper());
+//        // 将自定义消息转换器放入容器中，并设置在最前方，最先执行
+//        converters.add(0,converter);
+//    }
+}
