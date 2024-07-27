ec95c9ece6e7548d8ace8586f18f25dccfcc86a8
huyukang <1367213168@qq.com>
2024-07-27 19:06:00 +0800

    更新了一个能够接收多张图片上传的接口

--- mfac/src/main/java/com/mfac/controller/admin/FileController.java
+++ mfac/src/main/java/com/mfac/controller/admin/FileController.java
@@ -4,13 +4,13 @@ import com.mfac.pojo.Result;
 import com.mfac.properties.AliOssProperties;
 import com.mfac.util.AliOssUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Slf4j
@@ -37,6 +37,35 @@ public class FileController {
                 aliOssProperties.getBucketName(),
                 aliOssProperties.getEndpoint(), aliOssProperties.getAccessKeyId(), aliOssProperties.getAccessKeySecret());
         return Result.success(filePath);
+    }
+
+    /**
+     * 接收批量上传的图片
+     * @param files
+     * @return
+     */
+    @PostMapping("/batchImages")
+    public Result batchImages(@RequestParam Map<String, MultipartFile> files) {
+        HashMap<Integer, String> resultMap = new HashMap<>();
+        for (int i=1;i<=files.size();i++) {
+            MultipartFile file = files.get(String.valueOf(i));
+            if (file.isEmpty()) {
+                return Result.error("图片上传异常，请重试");
+            }
+            try {
+                String originalName = file.getOriginalFilename();
+                String extension = originalName.substring(originalName.lastIndexOf("."));
+                String uploadName = UUID.randomUUID().toString()+extension;
+                String filePath = AliOssUtil.upload(file.getBytes(), uploadName,
+                        aliOssProperties.getBucketName(),
+                        aliOssProperties.getEndpoint(), aliOssProperties.getAccessKeyId(), aliOssProperties.getAccessKeySecret());
+                resultMap.put(i, filePath);
+            } catch (Exception e) {
+                log.info("FileController => batchImages : {}", e);
+                return Result.error("图片上传异常，请重试");
+            }
+        }
+        return Result.success(resultMap);
     }
 }
 
